90ce4db367d39ac097d37a4246777b36c1190668
Ross Hettel <ross@het.tel>
2016-10-17 14:04:02 -0500

    initial server commit

---
+++ server/server.js
@@ -0,0 +1,7 @@
+// endpoint to read in GPS coords
+// parse in from hologram webhook format
+// handle array of coords
+// save to database with date/time
+// default show last Location
+// show routes with date filters
+// also generate heatmap
